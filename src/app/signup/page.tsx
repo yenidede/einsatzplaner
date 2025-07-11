3870aa750f634f5057236778751c299fd0fe3f39
omre20 <oemer.yenidede@hak-bregenz.at>
2025-07-11 12:24:29 +0200

    Signup und Signin added

---
+++ src/app/signup/page.tsx
@@ -0,0 +1,11 @@
+
+import SignupForm from '@/features/auth/components/SignUpForm';
+
+export default function SignupPage() {
+  return (
+    <div className="min-h-screen bg-gray-50">
+        <SignupForm />
+    </div>
+  );
+}
+
