e51219d80ec956eb1c7eba32ef471c84a3b9273e
David Kathrein <david.kathrein@hak-bregenz.at>
2025-07-17 11:15:59 +0200

    added basic layout changes

--- src/app/(pages)/benutzeransicht/page.tsx
+++ src/app/(pages)/benutzeransicht/page.tsx
@@ -2,10 +2,13 @@ import React from "react";
 
 export default function Helferansicht() {
   return (
-    <main>
+    <>
       <h1>Helferansicht</h1>
-      <p>Hier können Helfer ihre Aufgaben und Informationen einsehen.</p>
+      <p className="text-slate-600 leading-7">
+        Hier kannst du dich bei Einsätzen eintragen. Organisationen werden
+        anschließend automatisch informiert.{" "}
+      </p>
       {/* Weitere Inhalte und Komponenten für die Helferansicht */}
-    </main>
+    </>
   );
 }
