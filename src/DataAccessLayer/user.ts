2c4f9293e70d33fb721dd1e5ccbfe37aea13da6a
David Kathrein <david.kathrein@hak-bregenz.at>
2025-07-21 21:54:07 +0200

    Fixed User.ts (dal) and improved it by adding select queries

--- src/DataAccessLayer/user.ts
+++ src/DataAccessLayer/user.ts
@@ -1,28 +1,44 @@
-import prisma from '@/lib/prisma';
-import { hash } from 'crypto';
+import prisma from "@/lib/prisma";
 
 export async function getUserByEmail(email: string) {
-    return await prisma.user.findUnique({
-        where: { email },
+  return await prisma.user.findUnique({
+    where: { email },
+    include: {
+      user_organization_role: {
         include: {
-            user_organization_role: {
-                include: {
-                    roles: true,
-                    organization: true
-                }
-            }
-        }
-    });
+          organization: {
+            select: {
+              id: true,
+              name: true,
+              helper_name_singular: true,
+              helper_name_plural: true,
+            },
+          },
+          user_role_assignment: {
+            include: {
+              roles: {
+                select: {
+                  id: true,
+                  name: true,
+                  abbreviation: true,
+                },
+              },
+            },
+          },
+        },
+      },
+    },
+  });
 }
 
-export async function createUserWithOrgAndRole(data: {
+export async function createUserWithOrgAndRoles(data: {
   email: string;
   firstname?: string;
   lastname?: string;
   password: string;
   phone?: string;
   orgId: string;
-  roleId: string;
+  roleIds: string[];
 }) {
   return prisma.user.create({
     data: {
@@ -32,17 +48,33 @@ export async function createUserWithOrgAndRole(data: {
       password: data.password,
       phone: data.phone,
       user_organization_role: {
-        create: [{
-          organization: { connect: { id: data.orgId } },
-          roles: { connect: { id: data.roleId } },
-        }],
+        create: [
+          {
+            org_id: data.orgId,
+            user_role_assignment: {
+              connect: data.roleIds.map((id) => ({ id })),
+            },
+          },
+        ],
       },
     },
     include: {
       user_organization_role: {
         include: {
-          organization: true,
-          roles: true,
+          organization: {
+            select: {
+              name: true,
+              helper_name_singular: true,
+              helper_name_plural: true,
+            },
+          },
+          user_role_assignment: {
+            include: {
+              roles: {
+                select: { name: true },
+              },
+            },
+          },
         },
       },
     },
@@ -50,86 +82,111 @@ export async function createUserWithOrgAndRole(data: {
 }
 
 export async function getUserByIdWithOrgAndRole(userId: string) {
-    return await prisma.user.findUnique({
-        where: { id: userId },
+  return await prisma.user.findUnique({
+    where: { id: userId },
+    include: {
+      user_organization_role: {
         include: {
-            user_organization_role: {
-                include: {
-                    organization: true,
-                    roles: true,
-                },
+          organization: true,
+          user_role_assignment: {
+            include: {
+              roles: true,
             },
+          },
         },
-    });
+      },
+    },
+  });
 }
 export async function getOrCreateOrganizationByName(name: string) {
-    let organization = await prisma.organization.findFirst({
-        where: { name }
+  let organization = await prisma.organization.findFirst({
+    where: { name },
+  });
+  if (!organization) {
+    organization = await prisma.organization.create({
+      data: { name },
     });
-    if (!organization) {
-        organization = await prisma.organization.create({
-            data: { name }
-        });
-    }
-    return organization;
+  }
+  return organization;
 }
 
-export async function getOrCreateRoleByName(name: string) {
-    let role = await prisma.roles.findFirst({
-        where: { name }
-    });
-    if (!role) {
-        role = await prisma.roles.create({
-            data: { name }
-        });
-    }
-    return role;
-
+export async function getUsersWithRolesByOrgId(orgId: string) {
+  return await prisma.user.findMany({
+    where: {
+      user_organization_role: {
+        some: {
+          organization: {
+            id: orgId,
+          },
+        },
+      },
+    },
+    include: {
+      user_organization_role: {
+        include: {
+          organization: {
+            select: { id: true, name: true },
+          },
+          user_role_assignment: {
+            include: {
+              roles: true,
+            },
+          },
+        },
+      },
+    },
+  });
 }
 
 export async function updateLastLogin(userId: string) {
-    return await prisma.user.update({
-        where: { id: userId },
-        data: {
-            last_login: new Date(),
-            updated_at: new Date(),
-        },
-    });
-  }
+  return await prisma.user.update({
+    where: { id: userId },
+    data: {
+      last_login: new Date(),
+      updated_at: new Date(),
+    },
+  });
+}
 
 //#region Reset Password DAL
-export async function updateUserResetToken(email: string, resetToken: string, resetTokenExpiry: Date) {
-    return await prisma.user.update({
-        where: { email },
-        data: {
-            resetToken,
-            resetTokenExpires: resetTokenExpiry,
-            updated_at: new Date(),
-        },
-    });
+export async function updateUserResetToken(
+  email: string,
+  resetToken: string,
+  resetTokenExpiry: Date
+) {
+  return await prisma.user.update({
+    where: { email },
+    data: {
+      resetToken,
+      resetTokenExpires: resetTokenExpiry,
+      updated_at: new Date(),
+    },
+  });
 }
-
 
 export async function getUserWithValidResetToken(token: string) {
-    return await prisma.user.findFirst({
-        where: {
-            resetToken: token,
-            resetTokenExpires: {
-                gt: new Date()
-            }
-        }
-    });
+  return await prisma.user.findFirst({
+    where: {
+      resetToken: token,
+      resetTokenExpires: {
+        gt: new Date(),
+      },
+    },
+  });
 }
 
-export async function resetUserPassword(user: { id: string }, hashedPassword: string) {
-    return await prisma.user.update({
-        where: { id: user.id },
-        data: {
-            password: hashedPassword,
-            updated_at: new Date(),
-            resetToken: null,
-            resetTokenExpires: null
-        }
-    });
+export async function resetUserPassword(
+  user: { id: string },
+  hashedPassword: string
+) {
+  return await prisma.user.update({
+    where: { id: user.id },
+    data: {
+      password: hashedPassword,
+      updated_at: new Date(),
+      resetToken: null,
+      resetTokenExpires: null,
+    },
+  });
 }
 //#endregion
