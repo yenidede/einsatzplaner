b927a0c15d83b8bfd81b76d2369eb225d4128a72
David Kathrein <david.kathrein@hak-bregenz.at>
2025-08-16 10:50:33 +0200

    installing deps for advanced toolbar

--- src/features/einsatz/queryKeys.ts
+++ src/features/einsatz/queryKeys.ts
@@ -1,9 +1,10 @@
-import { EinsatzCustomizable, EinsatzCustomizableFilter } from "./types";
+import { EinsatzCustomizable } from "./types";
 
 export const queryKeys = {
     einsaetze: (orgs: string[]) => ['einsatz', "list", orgs] as const,
     allEinsaetze: () => ['einsatz', "list"] as const,
-    einsaetzeFiltered: (filters: Partial<EinsatzCustomizableFilter>, sort: { id: string; order: string }, page: number, perPage: number) => ['einsatz', "list", "filtered", filters, sort, page, perPage] as const,
+    einsaetzeTableView: () => ['einsatz', "list", "table"] as const,
+    einsaetzeFiltered: (filters: Partial<EinsatzCustomizable>, sort: { id: string; order: string }, page: number, perPage: number) => ['einsatz', "list", "filtered", filters, sort, page, perPage] as const,
     detailedEinsatz: (id: string) => ['einsatz', "detail", id] as const,
     einsatzComments: (id: string) => ['einsatz', "detail", id, "comments"] as const,
     einsatzHelpers: (id: string) => ['einsatz', "detail", id, "helpers"] as const
