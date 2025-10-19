4c60b9c2d169a0f3b1f726598f21c6321a7e5edc
yenidede <oemer.yenidede@hak-bregenz.at>
2025-10-19 20:40:29 +0200

    code refactoring

--- src/features/pdf/components/BookingConfirmationPDF.tsx
+++ src/features/pdf/components/BookingConfirmationPDF.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Document, Page, Text, View, StyleSheet, Image } from '@react-pdf/renderer';
+import { Page, Text, View, StyleSheet, Image } from '@react-pdf/renderer';
 
 const styles = StyleSheet.create({
   page: { padding: 40, fontFamily: 'Helvetica', fontSize: 11 },
@@ -43,7 +43,6 @@ export const BookingConfirmationPDF: React.FC<Props> = ({ einsatz, options }) =>
   }) + ' Uhr';
 
   return (
-    <Document>
       <Page size="A4" style={styles.page}>
         {/* Header */}
         {showLogos && einsatz.organization?.logo_url && (
@@ -147,6 +146,5 @@ export const BookingConfirmationPDF: React.FC<Props> = ({ einsatz, options }) =>
           Dornbirner Sparkasse IBAN AT71 2060 2004 0004 9911 | BIC DOSPAT2DXXX
         </Text>
       </Page>
-    </Document>
   );
 };
