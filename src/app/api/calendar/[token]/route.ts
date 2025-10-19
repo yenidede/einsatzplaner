d26e499baac4a13885b8e25baed4989bc353d575
yenidede <oemer.yenidede@hak-bregenz.at>
2025-10-19 18:04:49 +0200

    PDF GEN

--- src/app/api/calendar/[token]/route.ts
+++ src/app/api/calendar/[token]/route.ts
@@ -1,6 +1,7 @@
 import { NextRequest, NextResponse } from "next/server";
 import prisma from "@/lib/prisma";
 import ical, {ICalEventStatus, ICalCalendarMethod} from "ical-generator";
+import { getOrganizationsByIds } from "@/features/organization/org-dal";
 
 export async function GET(request: NextRequest,{params}: {params : {token : string}}){
     const subscription = await prisma.calendar_subscription.findUnique({
@@ -25,7 +26,7 @@ export async function GET(request: NextRequest,{params}: {params : {token : stri
         name: subscription.organization.name ?? "Einsatzplaner - Kalender",
         timezone: "Europe/Vienna",
         method: ICalCalendarMethod.PUBLISH,
-        ttl: 60*60*2, // 2 Cache Hinweis
+        ttl: 60*60*2,
         prodId: {
             company: "Einsatzplaner", 
             product: "Calendar", 
@@ -41,7 +42,9 @@ export async function GET(request: NextRequest,{params}: {params : {token : stri
         .filter(Boolean) ?? [];
 
         const ortField = einsatz.einsatz_field.find(field => (field.field.name ?? "")
-        .toLowerCase().match("/^(ort|location)$"));
+        .toLowerCase().match("/^(ort|location)$/"));
+
+        
 
         const urlToEinsatzPage = `${process.env.NEXTAUTH_URL}/einsaetze/${einsatz.id}`
 
@@ -60,6 +63,7 @@ export async function GET(request: NextRequest,{params}: {params : {token : stri
             url: urlToEinsatzPage,
             location: ortField?.value,
             status: ICalEventStatus.CONFIRMED,
+            phone: phoneField?.value,
         });
 
         event.uid(`${einsatz.id}@${host}`)
