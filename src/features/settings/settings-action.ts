4ab88ca584650532aa7aa5d6013f8c962f70817e
omre20 <oemer.yenidede@hak-bregenz.at>
2025-11-19 12:54:59 +0100

    commit

--- src/features/settings/settings-action.ts
+++ src/features/settings/settings-action.ts
@@ -10,7 +10,7 @@ import { OrganizationRole } from "@/types/next-auth";
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
+const supabaseServiceRoleKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 if (!supabaseUrl) {
   throw new Error("Environment variable NEXT_PUBLIC_SUPABASE_URL is not set.");
 }
