5d69ed3ea5aeda8b4a6b96e2e94eb4597d330f92
yenidede <oemer.yenidede@hak-bregenz.at>
2025-11-19 23:57:54 +0100

    settings-action SERVICE KEY changed

--- src/features/settings/settings-action.ts
+++ src/features/settings/settings-action.ts
@@ -10,14 +10,12 @@ import { OrganizationRole } from "@/types/next-auth";
 import { createClient } from "@supabase/supabase-js";
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
-const supabaseServiceRoleKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
 if (!supabaseUrl) {
   throw new Error("Environment variable NEXT_PUBLIC_SUPABASE_URL is not set.");
 }
 if (!supabaseServiceRoleKey) {
-  throw new Error(
-    "Environment variable NEXT_PUBLIC_SUPABASE_ANON_KEY is not set."
-  );
+  throw new Error("Environment variable SUPABASE_SERVICE_ROLE_KEY is not set.");
 }
 const supabase = createClient(supabaseUrl, supabaseServiceRoleKey);
 async function checkUserSession() {
