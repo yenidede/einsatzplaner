b257b9aa9cfb41bf94c2afc3d225b7c51d5a5ee5
omre20 <oemer.yenidede@hak-bregenz.at>
2025-11-27 16:40:24 +0100

    prisma.config.ts

--- src/app/(pages)/einsatztemplates/page.tsx
+++
@@ -1,9 +0,0 @@
-import EinsatzTemplateManager from '@/features/einsatztemplate/components/EinsatzTemplateManager';
-
-export default function EinsatztemplatesPage() {
-  return (
-    <div className="min-h-screen bg-gray-100 py-10">
-      <EinsatzTemplateManager />
-    </div>
-  );
-}
