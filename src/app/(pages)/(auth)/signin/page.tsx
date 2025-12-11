de0c1785016d27a552d074e364b8f31d3fd9f078
David Kathrein <david.kathrein@hak-bregenz.at>
2025-12-11 22:11:24 +0100

    relocated signup file

--- src/app/(pages)/(auth)/signin/page.tsx
+++ src/app/(pages)/(auth)/signin/page.tsx
@@ -45,7 +45,7 @@ export default function SignInPage() {
   };
 
   return (
-    <div className="min-h-screen bg-gray-50 flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
+    <div className="bg-secondary grow flex items-center justify-center py-12 px-4 sm:px-6 lg:px-8">
       <div className="max-w-md w-full space-y-8">
         <div>
           <h2 className="mt-6 text-center text-3xl font-extrabold text-foreground">
