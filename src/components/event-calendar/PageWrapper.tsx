ebd8a9fd3b659aa4c30ba1eaea5816479ee10daf
David Kathrein <david.kathrein@hak-bregenz.at>
2025-12-16 10:54:10 +0100

    started splitting helferansicht and einsatzverw

---
+++ src/components/event-calendar/PageWrapper.tsx
@@ -0,0 +1,66 @@
+"use client";
+
+import React from "react";
+import Calendar from "@/components/event-calendar/calendar";
+import { useSession } from "next-auth/react";
+import { queryKeys as orgsQueryKeys } from "@/features/organization/queryKeys";
+import { queryKeys as einsatzQueryKeys } from "@/features/einsatz/queryKeys";
+import { getOrganizationsByIds } from "@/features/organization/org-dal";
+import { useQuery } from "@tanstack/react-query";
+import { getEinsaetzeData } from "@/components/event-calendar/utils";
+import { CalendarMode } from "./types";
+
+export default function CalendarPageWrapper({
+  mode,
+  description,
+}: {
+  mode: CalendarMode;
+  description?: string;
+}) {
+  const { data: session } = useSession();
+  const orgIds = session?.user.orgIds;
+
+  const { data: organizations } = useQuery({
+    queryKey: orgsQueryKeys.organizations(orgIds ?? []),
+    queryFn: () => getOrganizationsByIds(orgIds ?? []),
+    enabled: !!orgIds?.length,
+  });
+
+  const activeOrgId = session?.user?.activeOrganization?.id;
+  const activeOrg =
+    organizations?.find((org) => org.id === activeOrgId) ?? null;
+
+  const { data: events } = useQuery({
+    queryKey: einsatzQueryKeys.einsaetze(activeOrgId ? [activeOrgId] : []),
+    queryFn: () => {
+      console.log(activeOrgId);
+      return getEinsaetzeData(activeOrgId);
+    },
+    enabled: !!activeOrgId,
+  });
+
+  const descriptionText = description
+    ? description
+    : mode === "verwaltung"
+    ? activeOrg?.verwalteransicht_description ??
+      `Hier kannst du dich für ${
+        activeOrg?.einsatz_name_plural ?? "Einsätze"
+      } eintragen. Bitte wähle die Termine aus, an denen du verfügbar bist.`
+    : activeOrg?.helferansicht_description ??
+      `Hier siehst du alle verfügbaren ${
+        activeOrg?.einsatz_name_plural ?? "Einsätze"
+      }. Bitte trage dich für die Termine ein, an denen du helfen kannst.`;
+
+  if (!events) {
+    return <div>Lade Einsätze...</div>;
+  }
+  return (
+    <>
+      <h1>{activeOrg?.einsatz_name_plural ?? "Einsätze"}</h1>
+      <p className="text-muted-foreground leading-7">{descriptionText}</p>
+      <div className="mt-6">
+        <Calendar mode={mode} />
+      </div>
+    </>
+  );
+}
