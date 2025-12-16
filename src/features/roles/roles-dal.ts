1b3325f11357b69855f35b73a017e3da05b4b274
David Kathrein <david.kathrein@hak-bregenz.at>
2025-12-16 18:11:34 +0100

    code review fixes

--- src/features/roles/roles-dal.ts
+++ src/features/roles/roles-dal.ts
@@ -4,12 +4,17 @@ import prisma from "@/lib/prisma";
 import { unstable_cache as cache, revalidateTag } from "next/cache";
 import type { role as Role } from "@/generated/prisma";
 
-export async function getRolesValuePairs() {
-  return cache(
-    async (): Promise<Role[]> => prisma.role.findMany(),
-    ["roles"],
-    { tags: ["roles"], revalidate: 3600 } // 1h TTL
-  )();
+export async function getAllRoles() {
+  try {
+    return cache(
+      async (): Promise<Role[]> => prisma.role.findMany(),
+      ["roles"],
+      { tags: ["roles"], revalidate: 3600 } // 1h TTL
+    )()
+  } catch (error) {
+    console.error("Error fetching roles:", error);
+    throw error;
+  }
 }
 
 // call this after any role create/update/delete
