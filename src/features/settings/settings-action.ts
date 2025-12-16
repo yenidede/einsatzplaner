0fc7b140c13d376fb35b6b5d769239888c8c2fd8
omre20 <oemer.yenidede@hak-bregenz.at>
2025-12-16 09:04:32 +0100

    Removed unused import

--- src/features/settings/settings-action.ts
+++ src/features/settings/settings-action.ts
@@ -8,7 +8,7 @@ import { hash, compare } from "bcrypt";
 import { revalidatePath } from "next/cache";
 import { OrganizationRole } from "@/types/next-auth";
 import { createClient } from "@supabase/supabase-js";
-import { toast } from "sonner";
+
 
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseServiceRoleKey = process.env.SUPABASE_SERVICE_ROLE_KEY;
