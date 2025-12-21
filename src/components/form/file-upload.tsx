ff52fee542628a91f8ed83cd65c5036c271f2b6a
David Kathrein <david.kathrein@hak-bregenz.at>
2025-12-21 22:35:42 +0100

    invite and register new UI/UX

--- src/components/form/file-upload.tsx
+++ src/components/form/file-upload.tsx
@@ -116,7 +116,6 @@ export function FileUpload({
           data-dragging={isDragging || undefined}
           className="border-input hover:bg-accent/50 data-[dragging=true]:bg-accent/50 has-[input:focus]:border-ring has-[input:focus]:ring-ring/50 flex min-h-32 flex-col items-center justify-center rounded-md border border-dashed p-4 transition-colors has-disabled:pointer-events-none has-disabled:opacity-50 has-[input:focus]:ring-[3px] hover:cursor-pointer"
         >
-          {/* @ts-expect-error caused when upgrading to Nextjs 16 */}
           <input
             {...getInputProps()}
             className="sr-only"
