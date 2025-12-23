986111381b24e3a951b879c2b503895e36d23af8
yenidede <oemer.yenidede@hak-bregenz.at>
2025-12-23 23:50:34 +0100

    TestComm

--- src/app/(pages)/(auth)/signup/page.tsx
+++ src/app/(pages)/(auth)/signup/page.tsx
@@ -3,9 +3,7 @@ import SignUpForm from "../../../../features/auth/components/acceptAndRegister-F
 export default function SignupPage() {
   return (
     <div className="bg-secondary flex grow flex-col items-center justify-center p-6 md:p-10">
-      <div className="w-full max-w-sm md:max-w-4xl">
-        <SignUpForm />
-      </div>
+      <div className="w-full max-w-sm md:max-w-4xl">{<SignUpForm />}</div>
     </div>
   );
 }
